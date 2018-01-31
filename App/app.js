63c112279df588f658539d96d14b627e7bf4c3a1
freddedotme <d6gg0vxb7w27r1ks@icloud.com>
2018-01-31 14:06:10 +0100

    changes to gitignore, default app.js fixed.

---
+++ App/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+
+var port = 1337;
+app.set('port', (process.env.PORT || port));
+
+app.use(express.static(path.join(__dirname, 'public/')));
+
+// client route
+app.get('/client', function (req, res) {
+    res.sendFile(path.join(__dirname, 'views/client/index.html'));
+});
+
+// driver route
+app.get('/driver', function (req, res) {
+    res.sendFile(path.join(__dirname, 'views/driver/index.html'));
+});
+
+// dispatcher route
+app.get('/dispatcher', function (req, res) {
+    res.sendFile(path.join(__dirname, 'views/dispatcher/index.html'));
+});
+
+var server = http.listen(app.get('port'), function () {
+    console.log('Server listening on port ' + app.get('port'));
+})
