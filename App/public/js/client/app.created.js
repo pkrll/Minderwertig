09c16085ef4d27c97b9d66ce8742f8f4c13f682d
Daniel Degirmen <dannebra@gmail.com>
2018-02-11 12:39:14 +0100

    Original

--- App/public/js/client/app.created.js
+++ App/public/js/client/app.created.js
@@ -1,5 +1,6 @@
 const created = function () {
-  socket.emit('client/login', {email: "a@a.se", password: "foo"});
+
+  //socket.emit('client/login', {email: "a@a.se", password: "foo"});
 
   /**
    * Invoked on successful login.
@@ -8,7 +9,7 @@ const created = function () {
    */
   socket.on('login/success', function (account) {
     this.account = account;
-    router.push('/client/menu');
+    //router.push('/client/order');
   }.bind(this));
   /**
    * Invoked when login has failed.
@@ -16,34 +17,15 @@ const created = function () {
    * @param  {Object} message The reason for failure.
    */
   socket.on('login/failure', function (message) {
-    this.temporary.message = message;
+    this.message = message;
     router.push('/client/login/fail');
   }.bind(this));
   /**
-   * Invoked when receiving a new booking confirmation.
+   * Invoked when received a new booking confirmation.
    *
    * @param  {Object} data The booking details.
    */
-  socket.on('trip/proposal', function (data) {
-    this.temporary.currentOrder = data;
-    router.push('/client/order/confirmation');
-  }.bind(this));
-  /**
-   * Invoked when client receives a new trip.
-   *
-   * @param  {Object} trip The trip.
-   */
-  socket.on('trip/new', function (trip) {
-    delete this.temporary.currentOrder;
-    this.trips.push(trip);
-    router.push('/client/trips');
-  }.bind(this));
-  /**
-   * Invoked when client receives an error from the server.
-   *
-   * @param  {Object} data.message The error message
-   */
-  socket.on('error', function (data) {
-    alert(data.message);
+  socket.on('order/booking', function (data) {
+    console.log(data);
   });
 }
